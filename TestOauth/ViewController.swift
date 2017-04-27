c247b451607f01b87379699c0c757255c065b8a1
Dante Solorio <ddasogaa@gmail.com>
2017-04-27 13:56:42 -0700

    Testing with Twitter app

--- TestOauth/ViewController.swift
+++ TestOauth/ViewController.swift
@@ -7,12 +7,37 @@
 //
 
 import UIKit
+import OAuthSwift
 
 class ViewController: UIViewController {
+    
+    
+    var oauthswift: OAuth1Swift?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        oauthswift = OAuth1Swift(
+            consumerKey:    "VTkUzSX4Yqzlzlu6MBLdcIJoP",
+            consumerSecret: "1TdkkuboBIJjEOZsOk7NIiFlQbLtfAf1Lbvgqp27qLfGLXDACf",
+            requestTokenUrl: "https://api.twitter.com/oauth/request_token",
+            authorizeUrl:    "https://api.twitter.com/oauth/authorize",
+            accessTokenUrl:  "https://api.twitter.com/oauth/access_token"
+        )
+        
+        let handle = oauthswift?.authorize(
+            withCallbackURL: URL(string: "TestOauth://testoauth/twitter")!,
+            success: { credential, response, parameters in
+                print(credential.oauthToken)
+                print(credential.oauthTokenSecret)
+                print(parameters["user_id"])
+        },
+            failure: { error in
+                print(error.localizedDescription)
+        }             
+        )
     }
 
     override func didReceiveMemoryWarning() {
